380c43dbadcb297113233f74706d43b4ce716ce5
Ler Wilson <lws803@gmail.com>
2017-10-08 03:01:30 +0800

    Added shaker sort

--- CheatSheets/sortingAlgosTimeTest.c
+++ CheatSheets/sortingAlgosTimeTest.c
@@ -6,12 +6,13 @@ int bubbleSort (int[], int);
 int insertionSort (int[], int);
 int selectionSort (int[], int);
 int enhancedBubble (int[], int);
+int shakerSort(int[], int);
 
 int main () {
 	int choice;
 	int arr1[1000], arr2[2000], arr4[4000], arr8[8000], arr16[16000];
 
-	printf("1: bubblesort, 2: insertion sort, 3: selection sort, 4: Enhanced bubblesort\n");
+	printf("1: bubblesort, 2: insertion sort, 3: selection sort, 4: Enhanced bubblesort, 5: Shaker sort\n");
 
 	printf("What sort you want?: ");
 	scanf("%d", &choice);
@@ -52,6 +53,15 @@ int main () {
 		printf("size 8000 : %d \n", enhancedBubble(arr8, 8000));
 		printf("size 16000 : %d \n", enhancedBubble(arr16, 16000));
 		printf("-----Best case: O(n), worst case: O(n^2)-----\n");
+		break;
+		case 5:
+		printf("Shaker sort selected\n");
+		printf("size 1000 : %d \n", shakerSort(arr1, 1000));
+		printf("size 2000 : %d \n", shakerSort(arr2, 2000));
+		printf("size 4000 : %d \n", shakerSort(arr4, 4000));
+		printf("size 8000 : %d \n", shakerSort(arr8, 8000));
+		printf("size 16000 : %d \n", shakerSort(arr16, 16000));
+		printf("-----Best case: O(n), worst case: O(n^2)-----\n"); 
 		break;
 		default:
 		printf("Bubble sort selected\n");
@@ -154,4 +164,34 @@ int enhancedBubble (int arr[], int n) {
 	finish = clock();
 
 	return (int)(finish - start);
+}
+
+
+int shakerSort(int array[], int size) {
+	clock_t start, finish;
+	int swapped = 0;
+	start = clock();	
+    for (int i = 0; i < size/2; i++) {
+    	swapped = 0;
+        for (int j = i; j < size - i - 1; j++) { //one way
+            if (array[j] < array[j+1]) {
+                int tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                swapped = 1;
+            }
+        }
+        for (int j = size - 2 - i; j > i; j--) { //and back
+            if (array[j] > array[j-1]) {
+                int tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) break;
+    }
+    finish = clock();
+
+    return (int)(finish - start);
 }
