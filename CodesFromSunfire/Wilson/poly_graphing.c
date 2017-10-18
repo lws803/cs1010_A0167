bfcf87b181417cd93fb1dd2cf9bd23dda44ff68f
Ler Wilson <lws803@gmail.com>
2017-10-19 02:04:48 +0800

    My version of the poly graphing using matrix. Might be a little more complicated

--- CodesFromSunfire/Wilson/poly_graphing.c
+++ CodesFromSunfire/Wilson/poly_graphing.c
@@ -7,15 +7,18 @@ double polynomial (double);
 void plotGraph (char [YMAX][XMAX], double, double);
 void display (char [YMAX][XMAX]);
 void init (char [YMAX][XMAX]);
-
+double findMin (double , double );
+double findMax (double , double );
+void plant_Y_axis (char [YMAX][XMAX], double, double);
+void plant_X_axis (char [YMAX][XMAX], double, double);
 
 int main () {
+	double rangeX_1, rangeX_2;
 	char matrix[YMAX][XMAX];
-
-
 	init(matrix);
-
-	plotGraph(matrix, -5.1, 5.0);
+	printf("Enter range: ");
+	scanf("%lf %lf", &rangeX_1, &rangeX_2);
+	plotGraph(matrix, rangeX_1, rangeX_2);
 
 	display (matrix);
 }
@@ -39,33 +42,96 @@ void display (char matrix[YMAX][XMAX]) {
 	}
 }
 
-
 void plotGraph (char matrix[YMAX][XMAX], double rangeX_1, double rangeX_2) {
-	double i, min;
-	int d;
+	double i, min, max, y_range;
+	int d, c;
 
 	double increment = fabs(rangeX_2 - rangeX_1)/XMAX;
+	max = findMax(rangeX_1, rangeX_2);
+	min = findMin(rangeX_1, rangeX_2);
 
-	double Y_range = fabs(polynomial(rangeX_2) - polynomial(rangeX_1));
+	plant_Y_axis(matrix, rangeX_1, rangeX_2);
+	plant_X_axis(matrix, rangeX_1, rangeX_2);
+	printf("Min: %lf Max: %lf\n", min, max);
+	// TODO: Add the axis 
 
-	// Find min 
 
-	if (polynomial(rangeX_1) > polynomial(rangeX_2)) {
-		min = polynomial(rangeX_2);
-	}else{
-		min = polynomial(rangeX_1);
+	for (i = rangeX_1, d = 0; i < rangeX_2; i += increment, d++) {
+		// Ratio 
+		int Y = ((polynomial(i)-max)/(min-max))*(YMAX-1);
+		matrix[(YMAX-1) - Y][d] = 'x';
 	}
 
-	// TODO: Fix the int Y position determinant
-	for (i = rangeX_1, d = 0; i <= rangeX_2; i += increment, d++) {
-		int Y = (YMAX-1) - (fabs(polynomial(i) - min)/Y_range)*(YMAX-1); 
-		matrix[Y][d] = 'x';
 
-	}
 }
 
 double polynomial (double x) {
-	double y = x*x*x + 2*x*x + 5.0;
-
+	double y = x*x*x + 2*x*x +5.0;
 	return y;
+}
+
+double findMin (double rangeX_1, double rangeX_2) {
+	double i, min;
+	min = polynomial(rangeX_1);
+	double increment = fabs(rangeX_2 - rangeX_1)/XMAX;
+
+	for (i = rangeX_1; i < rangeX_2; i += increment) {
+		if (polynomial(i) < min) {
+			min = polynomial(i);
+		}
+	}
+	return min;
+}
+
+double findMax (double rangeX_1, double rangeX_2) {
+	double i, max;
+	max = polynomial(rangeX_1);
+	double increment = fabs(rangeX_2 - rangeX_1)/XMAX;
+	for (i = rangeX_1; i < rangeX_2; i += increment) {
+		if (polynomial(i) > max) {
+			max = polynomial(i);
+		}
+	}
+	return max;
+}
+
+void plant_Y_axis (char matrix[YMAX][XMAX], double rangeX_1, double rangeX_2) {
+	int d, index;
+	double increment = fabs(rangeX_2 - rangeX_1)/XMAX;
+	double i, closestTo = rangeX_1;
+	if (rangeX_1 <= 0 && rangeX_2 >= 0) {
+		for (i = rangeX_1, d = 0; i < rangeX_2; i += increment, d++) {
+			if (fabs(i) < fabs(closestTo)) {
+				closestTo = i;
+				index = d;
+			}
+		}
+		for (d = 0; d < YMAX; d++) {
+			matrix[d][index] = '|';
+		}
+	}
+}
+
+void plant_X_axis (char matrix[YMAX][XMAX], double rangeX_1, double rangeX_2) {
+	int index, d;
+	double max = findMax(rangeX_1, rangeX_2);
+	double min = findMin(rangeX_1, rangeX_2);
+	double increment = fabs(rangeX_2 - rangeX_1)/XMAX;
+	double i, closestTo = ((polynomial(rangeX_1)-max)/(min-max))*(YMAX-1);
+	if (findMin(rangeX_1, rangeX_2) <= 0 && findMax(rangeX_1, rangeX_2) >= 0) {
+		for (i = rangeX_1; i < rangeX_2; i += increment) {
+			if (fabs(polynomial(i)) < fabs(closestTo)) {
+				int Y = ((polynomial(i)-max)/(min-max))*(YMAX-1);
+				closestTo = polynomial(i);
+				index = (YMAX -1) - Y;
+			}
+		}
+		for (d = 0; d < XMAX; d++) {
+			if (matrix[index][d] == '|') {
+				matrix[index][d] = '+';
+			}else {
+				matrix[index][d] = '-';
+			}
+		}
+	}
 }
