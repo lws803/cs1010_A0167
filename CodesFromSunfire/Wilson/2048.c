1be7691bdff800ab362120030c3e872b436c3ddd
Ler Wilson <lws803@gmail.com>
2017-10-22 22:26:05 +0800

    Interesting question on the game of 2048

---
+++ CodesFromSunfire/Wilson/2048.c
@@ -0,0 +1,138 @@
+// CS1010 AY2014/5 Semester 1
+// PE2 Ex2: 2048.c
+// Name: LER WEI SHENG
+// Matriculation number: 
+// plab-id: 
+// Discussion group: 
+// Description: 
+
+#include <stdio.h>
+#define SIZE 4
+#define LENGTH 21
+
+void readGrid(int [][SIZE]); 
+void printGrid(int [][SIZE]);
+int exist (int [][SIZE], int); 
+void moveUp (int [][SIZE]); 
+void moveLeft (int [][SIZE]);
+int play(int [][SIZE], char [LENGTH], int);
+
+int main(void) {
+	int grid[SIZE][SIZE], target;
+	char directions[LENGTH];
+
+	printf("Enter grid:\n");
+	readGrid(grid);
+	
+	printf("Enter directions: ");
+	scanf("%s", directions);
+
+	printf("Enter target: ");
+	scanf ("%d", &target);
+
+	if (play(grid, directions, target)) {
+		printGrid(grid);
+		printf("%d is formed.\n", target);
+	}else {
+		printGrid(grid);
+		printf("%d is not formed.\n", target);
+
+	}
+
+	return 0;
+}
+
+// Read in the grid
+void readGrid(int grid[][SIZE]) {
+	int i,j;
+
+	for (i=0; i<SIZE; i++)
+		for (j=0; j<SIZE; j++)
+			scanf("%d",&grid[i][j]);
+}
+
+// Print out the grid
+void printGrid(int grid[][SIZE]) {
+	int i,j;
+
+	for (i=0; i<SIZE; i++) {
+		for (j=0; j<SIZE; j++)
+			printf("%d ",grid[i][j]);
+		printf("\n");
+	}
+}
+// If the number exists, then return 1; 
+int exist (int matrix[][SIZE], int number) {
+	int i, d; 
+	for (i = 0; i < SIZE; i++) {
+		for (d = 0; d < SIZE; d++) {
+			if (matrix[i][d] == number) {
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+// Will play according to the instructions of numbers entered and checks everytime to see if the wanted number is formed. 
+int play(int matrix[][SIZE], char directions[], int number) {
+	int i; 
+	for (i = 0; directions[i] != '\0'; i++) {
+		switch (directions[i]) {
+			case 'U':
+			moveUp(matrix);
+			break;
+			case 'L':
+			moveLeft(matrix);
+			break;
+		}
+		if (exist(matrix, number)) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// Move the board upwards and break the backwards traversal of 'c' if an addition is made 
+void moveUp (int matrix[][SIZE]) {
+	// Do we have to ensure that the addition is done only in one step? 
+	int i, d, c; 
+	for (i = 0; i < SIZE; i++) {
+		for (d = 1; d < SIZE; d++) {
+			for (c = d; c > 0; c--) {
+				if (matrix[c - 1][i] == 0) {
+					// Perform moving up
+					matrix[c - 1][i] = matrix[c][i];
+					matrix[c][i] = 0;
+				} else if (matrix[c][i] == matrix[c - 1][i]) {
+					// Move up and add 
+					matrix[c-1][i] += matrix[c][i];
+					matrix[c][i] = 0;
+					break;
+				} 		
+			}
+		}
+	}
+}
+
+// Move the board leftwards and break the backwards traversal of 'c' if an addition is made 
+void moveLeft (int matrix[][SIZE]) {
+	// Do we have to ensure that the addition is done only in one step? 
+	int i, d, c; 
+	for (i = 0; i < SIZE; i++) {
+		for (d = 1; d < SIZE; d++) {
+			for (c = d; c > 0; c--) {
+				if (matrix[i][c - 1] == 0) {
+					// Perform moving left
+					matrix[i][c-1] = matrix[i][c];
+					matrix[i][c] = 0;
+				} else if (matrix[i][c] == matrix[i][c-1]) {
+					// Move left and add 
+					matrix[i][c-1] += matrix[i][c];
+					matrix[i][c] = 0;
+					break;
+				} 		
+			}
+		}
+	}
+}
