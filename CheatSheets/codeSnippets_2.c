c96cb283693cc6a2b999c777a9c0b23371ddb14b
Ler Wilson <lws803@gmail.com>
2017-11-17 16:35:48 +0800

    Added method of obtaining subsets of a set

--- CheatSheets/codeSnippets_2.c
+++ CheatSheets/codeSnippets_2.c
@@ -573,3 +573,28 @@ void fill (char matrix[][MAX], int x, int y) {
 	if (y+1 < MAX && matrix[y+1][x] != 'x')
 		fill (matrix, x, y+1);
 }
+
+
+// Functions to try out different combinations of r element subsets 
+void subset(int arr[], int data[], int start, int end, int index, int r)
+{
+    int j, i;
+    if (index == r) {
+        for (j = 0; j < r; j++)
+            printf("%d ", data[j]);
+        printf("\n");
+        // Add any operations here to process data (subset)
+        return;
+    }
+    for (i = start; i <= end && end - i + 1 >= r - index; i++)
+    {
+        data[index] = arr[i];
+        subset(arr, data, i+1, end, index+1, r);
+    }
+}
+ 
+void printsubset(int arr[], int n, int r)
+{
+    int data[r];
+    subset(arr, data, 0, n - 1, 0, r);
+}
