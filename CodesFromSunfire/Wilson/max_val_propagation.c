7ce1872e91c61b80d889e6f54b198a92a776f43c
Ler Wilson <lws803@gmail.com>
2017-11-20 12:19:28 +0800

    Updated to full question

--- CodesFromSunfire/Wilson/max_val_propagation.c
+++ CodesFromSunfire/Wilson/max_val_propagation.c
@@ -2,16 +2,25 @@
 
 void maxPush (int[], int, int, int); 
 
+int maxPull (int arr[], int size, int index, int length); 
+
 
 
 int main(int argc, char const *argv[])
 {
 	int arr[10] = {7,3,8,5,7,10,9,12,6,5};
+	int arr2[10] = {11,3,5,8,19,7,10,12,6,9};
+
 	maxPush (arr, 10, 3, 7);
+	maxPull (arr2, 10, 0, 4);
 
 	int i;
 	for (i = 0; i < 10; i++) 
 		printf ("%d ", arr[i]);
+	printf("\n");
+
+	for (i = 0; i < 10; i++) 
+		printf ("%d ", arr2[i]);
 	printf("\n");
 	return 0;
 }
@@ -23,4 +32,14 @@ void maxPush (int arr[], int size, int index, int length) {
 		}
 		maxPush (arr, size, index+1, length-1);
 	}
+}
+
+int maxPull (int arr[], int size, int index, int length) {
+	if (length > 1 && index < size) {
+		int next = maxPull (arr, size, index+1, length-1);
+		if (next > arr[index]) {
+			arr[index] = next;
+		}
+	}
+	return arr[index];
 }
